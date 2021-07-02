cda059ea978b0e095927384a368894de9de10d5a
airhac <oooh32141@gmail.com>
2021-07-02 11:07:46 +0900

    7/2 prac pro2 3

--- postapp/views.py
+++ postapp/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'postapp/hello_world.html')
 # Create your views here.

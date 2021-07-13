29d6d5e990c973e70ec318cff778db72c23101ea
airhac <oooh32141@gmail.com>
2021-07-13 09:33:21 +0900

    7/13 prac pro2 1 1. Hello_world.html에서 input의 이름을 지정, text의 값을 model_instance값을 가져와 준다. 2. view에서 input으로 바등ㄴ 값을 moidel에  저장하주고 hello_world.html로 그 값을 보내준다. 3. model에 값을 저장한다.

--- postapp/views.py
+++ postapp/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from postapp.models import NewModel
+
 
 def hello_world(request):
     if request.method == 'POST':
-        return render(request, 'postapp/hello_world.html', context={'text':'POST METHOD!'})
+        temp = request.POST.get('input_text')
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
+
+        return render(request, 'postapp/hello_world.html', context={'model_instance':model_instance})
     else:
         return render(request, 'postapp/hello_world.html', context={'text':'GET METHOD!'})
 # Create your views here.
